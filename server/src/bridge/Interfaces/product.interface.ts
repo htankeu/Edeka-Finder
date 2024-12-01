8d343b111e43049a871c20144aeabf645a27bf84
Joel Heda <hedayvan@gmail.com>
2024-12-02 00:53:24 +0100

    Initialize User Interface with Astrojs framework

---
+++ server/src/bridge/Interfaces/product.interface.ts
@@ -0,0 +1,6 @@
+import { IStorage } from "./storage.interface";
+
+export interface IProduct {
+  ProductName: string;
+  Storage: IStorage;
+}
---
+++ server/src/bridge/Interfaces/user.Interface.ts
@@ -0,0 +1,7 @@
+export default interface IUser {
+  Name: string;
+  Email: string;
+  Password: string;
+  Phonenumber: string;
+  Address: string;
+}
