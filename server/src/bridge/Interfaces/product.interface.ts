d61953026a2ed9cf0ef440d2ad7b2b7fa4f67054
Joel Heda <hedayvan@gmail.com>
2024-12-02 12:45:00 +0100

    Initialization of our system's server

--- server/src/bridge/Interfaces/product.interface.ts
+++ server/src/bridge/Interfaces/product.interface.ts
@@ -3,4 +3,5 @@ import { IStorage } from "./storage.interface";
 export interface IProduct {
   ProductName: string;
   Storage: IStorage;
+  Quantity: number;
 }
--- server/src/bridge/Interfaces/user.Interface.ts
+++ server/src/bridge/Interfaces/user.Interface.ts
@@ -4,4 +4,5 @@ export default interface IUser {
   Password: string;
   Phonenumber: string;
   Address: string;
+  Role: string;
 }
