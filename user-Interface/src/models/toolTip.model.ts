f069719f8c10b954a4b6bc32d80c34bc42077f74
Joel Heda <hedayvan@gmail.com>
2024-12-04 20:36:41 +0100

    German map added for the localization

---
+++ user-Interface/src/models/toolTip.model.ts
@@ -0,0 +1,6 @@
+export interface ToolTip {
+  text: string;
+  x: number;
+  y: number;
+}
+
--- user-Interface/src/pages/auth.page.tsx
+++ user-Interface/src/pages/auth.page.tsx
@@ -3,6 +3,7 @@ import Login from "../components/Auth-login.component";
 import Register from "../components/Auth-register.component";
 import HeaderNav from "../components/nav/Header-nav.component";
 import GermanMap from "../components/home/home-svg.component";
+import Rhinland from "../components/germany/rhinland.component";
 
 const AuthPage: React.FC = () => {
   const [isLogin, setIsLoging] = useState<boolean>(true);
@@ -15,6 +16,7 @@ const AuthPage: React.FC = () => {
       <div className="bg-zinc-800">
         <HeaderNav />
         {/* {isLogin ? <Login toogle={toogleAuth}/> : <Register toogle={toogleAuth} />} */}
+        {/* <Rhinland/> */}
         <GermanMap/>
       </div>
     </>
