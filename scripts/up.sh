48c8d99f67075ee8104822ac0f573ba090c96778
Joel Heda <hedayvan@gmail.com>
2024-12-18 14:04:53 +0100

    update the script for lauching a server

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,11 +1,6 @@
 cd ..
 docker compose up -d
 
-PGHOST=localhost   # Or the container's IP or hostname
-PGPORT=5432        # Default PostgreSQL port
-PGUSER=edekaFinder # PostgreSQL username
-# PGPASSWORD=edekafinder  # PostgreSQL password
-
 until docker compose logs DB | grep -q "database system is ready to accept connections"; do
     echo "Waiting for PostgreSQL to initialize..."
     sleep 2
